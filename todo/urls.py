874ccaeef359570057fc5773b63bc143393846b6
unknown <15-12-1991@mail.ru>
2021-01-17 23:55:31 +0300

    added test third

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include, path
-from main.views import homepage, test, second
+from main.views import homepage, test, second, third
 
 
 
@@ -25,7 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", homepage, name="home"),
     path("test", test, name="test"),
-    path("test2/", second)
+    path("test2/", second),
+    path("test3/", third)
 
     
 ]
